e1ea028193e180f37dd810e06a798cc3a5cccc47
schawnery <schawl@uw.edu>
2025-08-22 14:24:05 -0700

    small script changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './routes/Home';
-import Footer from './components/Footer';
+import Home from './routes/Home'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <Home />
-        <Footer />
+      <Home />
+      <Footer />
     </>
   )
 }
